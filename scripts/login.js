9a4086bd01391d057164b186f455ac9a1464ba85
Cristian Gomez <cristian.gomez@obsidiansoft.io>
2024-11-19 22:18:49 -0500

    Landing y login finalizados

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    $('#username').val(user.username);
+    if (user.perfilImageChecked) {
+        $('#userImage').attr('src', `assets/images/user-${user.perfilImageChecked}.png`);
+    }
+})
+
+const onLogin = () => {
+    sessionStorage.setItem('email', $('#email').val());
+    window.location.href = "services-managment.html";
+}
